21deecba83503c0e26f2b30f6df1f1f906111876
Andreja <stamenkovicandreja02@gmail.com>
2025-06-13 05:23:48 +0200

    Dodavanje Projekta

---
+++ Projekat/SW/Test/test_app/dump_log.c
@@ -0,0 +1,39 @@
+
+#include <stdint.h> // uint16_t and family
+#include <stdio.h> // printf and family
+#include <unistd.h> // file ops
+#include <fcntl.h> // open() flags
+#include <string.h> // strerror()
+#include <errno.h> // errno
+
+#include "motor_ctrl.h"
+
+
+int main(int argc, char** argv){
+
+	int fd;
+	fd = open(DEV_FN, O_RDWR);
+	if(fd < 0){
+		fprintf(stderr, "ERROR: \"%s\" not opened!\n", DEV_FN);
+		fprintf(stderr, "fd = %d %s\n", fd, strerror(-fd));
+		return 4;
+	}
+
+	log_t log_entry;
+
+	r = read(fd, (char*)&log_entry, sizeof(log_entry));
+	if(r != sizeof(log_entry)){
+		fprintf(stderr, "ERROR: read went wrong!\n");
+		return 4;
+	}
+
+	//TODO write log as textual TSV (Tab Separated Value) file
+
+	//TODO seek if needed.
+
+	close(fd);
+
+	printf("End.\n");
+
+	return 0;
+}
